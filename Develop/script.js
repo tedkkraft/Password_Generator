6f8fb10ddc7ea3193ffe13c5fbf6ab6a21c0e357
tedkkraft <tedkkraft@gmail.com>
2020-06-13 11:40:33 -0400

    Added getLength

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Get password length from user...
+function getLength() {
+  var userLength = prompt("Please enter the # of characters you'd like your password to be", "Enter a number between 8-128");
+  var passwordLength = parseInt(userLength);
+
+  //...then verify it is in range (8-128). If not in range, clear answer and prompt again.
+  if (passwordLength < 8 || passwordLength > 128) { 
+    userLength = null;
+    alert("Invalid entry")
+    getLength();
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
-  var passwordLength = prompt("Please enter the # of characters you'd like your password to be", "Enter a number between 8-128");
-    if (passwordLength !== null) {
-      function validateForm() {
-        var userLength = passwordLength.value;
-        if ( userLength < 8 || userLength > 128) {
-          alert("Please select a number between 8-128");
-          return false;
-    }}}
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+  getLength();
+} 
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

afe9d149fa8057ccba7c75e8ace64a5e69f763b4
tedkkraft <tedkkraft@gmail.com>
2020-06-13 13:01:44 -0400

    Stuck, need assistance

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Character arrays
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numeric = ["0","1","2","3","4","5","6","7","8","9","10"]
+var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+
 // Get password length from user...
 function getLength() {
   var userLength = prompt("Please enter the # of characters you'd like your password to be", "Enter a number between 8-128");
@@ -14,17 +20,36 @@ function getLength() {
   }
 }
 
+// Get character types from user...
+function getTypes() {
+  var lowerCheck = $("#lowercaseBox").is(":checked");
+  var upperCheck = $("#uppercaseBox").is(":checked");
+  var numericCheck = $("#numericBox").is(":checked");
+  var specialCheck = $("#specialBox").is(":checked");
+
+  if (lowerCheck = true) {
+
+  }
+}
+// Generate password
+function generatePassword() {
+  var i;
+  for (i = 0; i < passwordLength; i++) {
+
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
+  getLength();
+  getTypes();
+  generatePassword();
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  getLength();
 } 
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
